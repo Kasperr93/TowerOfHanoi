6727380487c32162dc336bdf3a5dcfbff8626189
Tomasz Kasperek <kasperek1993@gmail.com>
2018-11-28 14:21:57 +0100

    - Added Disk class and beginner implementation.

---
+++ src/kasperek/game/Disk.java
@@ -0,0 +1,24 @@
+package kasperek.game;
+
+/**
+ * @author Tomasz Kasperek
+ * @version 0.1 11/23/2018
+ * @since 0.1
+ */
+
+public class Disk {
+    private int size;
+
+    public Disk(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
