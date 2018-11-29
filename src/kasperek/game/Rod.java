99cedb118d1f0a59249655e25fba372af1547361
Tomasz Kasperek <kasperek1993@gmail.com>
2018-11-29 18:24:29 +0100

    - Changed Deque to Stack. - Override toString method.

--- src/kasperek/game/Rod.java
+++ src/kasperek/game/Rod.java
@@ -1,29 +1,33 @@
 package kasperek.game;
 
-import java.util.Deque;
-import java.util.List;
+import java.util.Stack;
 
 /**
  * @author Tomasz Kasperek
- * @version 0.1 11/27/2018
+ * @version 0.2 11/28/2018
  * @since 0.1
  */
 
 public class Rod {
-    private List<Deque<Disk>> ringsOnStake;
     private String name;
+    private Stack<Disk> disks;
 
-    public Rod(List<Deque<Disk>> ringsOnStake, String name) {
-        this.ringsOnStake = ringsOnStake;
+    public Rod(String name, Stack<Disk> disks) {
         this.name = name;
+        this.disks = disks;
     }
 
     public String getName() {
         return name;
     }
 
+
+    public Stack<Disk> getDisks() {
+        return disks;
+    }
+
     @Override
     public String toString() {
-        return super.toString();
+        return "Rod name: " + name + ", Number of disk: " + disks.size() + " Disk list: " + disks.toString();
     }
 }
