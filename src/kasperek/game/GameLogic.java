7b1d3dd86194ca8d5518cd08eafeb39f010e4532
Tomasz Kasperek <kasperek1993@gmail.com>
2018-12-06 00:40:42 +0100

    * Added steps counter; * Added methods:   - firstStep;   - secondStep;   - doTheSelectMove;   - endGame;

--- src/kasperek/game/GameLogic.java
+++ src/kasperek/game/GameLogic.java
@@ -2,16 +2,18 @@ package kasperek.game;
 
 import kasperek.utils.ConsoleInterfaces;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
  * @author Tomasz Kasperek
- * @version 0.1 11/29/2018
+ * @version 0.9 12/05/2018
  * @since 0.1
  */
 
 public class GameLogic {
     private Rod[] rods = new Rod[3];
+    private int steps = 0;
 
     public void generateGame(int numberOfDisk) {
         rods[0] = new Rod("Rod A", new Stack<>());
@@ -25,5 +27,52 @@ public class GameLogic {
         ConsoleInterfaces.displayGame(rods);
     }
 
+    public Rod firstStep() {
+        var rodNumber = ConsoleInterfaces.getFirstMove();
+        return rods[rodNumber - 1];
+    }
 
+    public Rod secondStep() {
+        var rodNumber = ConsoleInterfaces.getSecondMove();
+        return rods[rodNumber - 1];
+    }
+
+    public void doTheSelectMove(Rod fromRod, Rod toRod) {
+        var fromDiskSize = 0;
+        int toDiskSize;
+
+        try {
+            fromDiskSize = fromRod.getDisks().peek().getSize();
+        } catch (EmptyStackException e) {
+            ConsoleInterfaces.rodIsEmpty();
+            doTheSelectMove(firstStep(), secondStep());
+        }
+
+        try {
+            toDiskSize = toRod.getDisks().peek().getSize();
+        } catch (EmptyStackException e) {
+            toDiskSize = 0;
+        }
+
+
+        if (fromDiskSize < toDiskSize || toDiskSize == 0) {
+            Disk movedDisk = fromRod.getDisks().pop();
+            toRod.getDisks().push(movedDisk);
+
+            steps++;
+        } else {
+            ConsoleInterfaces.notAllowedMove();
+        }
+
+        ConsoleInterfaces.displayGame(rods);
+    }
+
+    public boolean endGame() {
+        if (rods[0].getDisks().isEmpty() && rods[1].getDisks().isEmpty()) {
+            ConsoleInterfaces.showResultGame(steps);
+            return true;
+        }
+
+        return false;
+    }
 }
